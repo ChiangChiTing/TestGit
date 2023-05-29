4980c2b0c7c554cc146dee22567a8c82bb551167
ryanchiang <falloutmax123@gmail.com>
2023-05-29 21:15:28 +0800

    Initial Commit

---
+++ TestGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestGit
+//
+//  Created by 姜霽庭 on 2023/5/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
