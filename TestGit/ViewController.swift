c07de1792443ca140cfd14d875edfd7b1bfb3fbb
ryanchiang <falloutmax123@gmail.com>
2023-05-29 22:00:29 +0800

    Hello -> Test

--- TestGit/ViewController.swift
+++ TestGit/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let message = "Hello Git!"
+    let message = "Test Git!"
 
         override func viewDidLoad() {
             super.viewDidLoad()
